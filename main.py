7605a61a970075f25e393a10a0229c64014066b2
dkozhukhar <424523+dkozhukhar@users.noreply.github.com>
2023-07-27 15:07:50 +0000

    yep

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 import openai
 import os
 
 app = Flask(__name__)
+app.secret_key = 'your secret key'
 
 try:
     openai.api_key = os.environ['OPENAI_API_KEY']
@@ -18,25 +19,27 @@ def index():
         system_message = request.form['system']
         user_message = request.form['message']
         if role == 'system':
-            messages = [
-                {"role": "system", "content": system_message}
-            ]
+            message = {"role": "system", "content": system_message}
         else:
-            messages = [
-                {"role": "system", "content": system_message},
-                {"role": "user", "content": user_message}
-            ]
+            message = {"role": "user", "content": user_message}
+        # If there is no conversation history in the session, create a new list
+        if 'history' not in session:
+            session['history'] = []
+        # Append the new message to the conversation history
+        session['history'].append(message)
         response = openai.ChatCompletion.create(
             model=model_name,
-            messages=messages
+            messages=session['history']
         )
+        # Append the assistant's message to the conversation history
+        session['history'].append({"role": "assistant", "content": response['choices'][0]['message']['content']})
         return jsonify({
             'message': response['choices'][0]['message']['content'],
             'usage': response['usage']
         })
+    # Clear the conversation history when loading the page
+    session.pop('history', None)
     return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
